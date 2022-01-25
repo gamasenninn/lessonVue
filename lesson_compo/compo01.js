4a15c3ff497c93e59904b34764487d312e703d09
gamasenninn <gamasenninn@yahoo.co.jp>
2022-01-25 14:04:49 +0900

    menuコンポ追加

--- lesson_compo/compo01.js
+++ lesson_compo/compo01.js
@@ -11,10 +11,21 @@ Vue.component('hello-world',{
 Vue.component('hello-world2',{
     template: `  
         <div>
-            <h1>2Hello World and {{ message }}</h1>   
+            <h1>2Hello World and {{ message }}</h1>
+            <p><b-button @click="increment">カウントアップ</b-button>　{{count}}</p>    
         </div>
         `,
-    props: ['message']
+    props: ['message'],
+    data(){
+        return{
+            count:0
+        }
+    },
+    methods:{
+        increment(){
+            this.count += 1
+        }
+    }
 });
 
 Vue.component('hello-world3',{
@@ -28,3 +39,41 @@ Vue.component('hello-world3',{
     props: ['message']
 });
 
+Vue.component('sc-menu',{
+    template: `  
+    <div>
+    <b-button v-b-toggle.sidebar-no-header>メニュー</b-button>
+    <b-sidebar id="sidebar-no-header" aria-labelledby="sidebar-no-header-title" no-header shadow>
+        <template #default="{ hide }">
+            <div class="p-3">
+                <h4 id="sidebar-no-header-title" style="float: left; width: 75%;">遷移メニュー</h4>
+                <b-button variant="danger" block @click="hide" style="float: right; width: 15%;">✖
+                </b-button>
+                <b-img src="https://free-materials.com/adm/wp-content/uploads/2020/10/logo_07-1024x1024.png"
+                    style="width: 100%; height: 50px; margin: 10px 0px;"></b-img>
+                <nav class="mt-3">
+                    <b-nav vertical class="mb-4">
+                        <b-button variant="info" block href="/home-page"><i class="fas fa-home"></i>　TOP
+                        </b-button>
+                    </b-nav>
+                    <b-nav vertical>
+                        <b-button variant="primary" block href="/customer-page"><i
+                                class="fas fa-building"></i>　得意先
+                        </b-button>
+                        <b-button variant="primary" block href="/item-page"><i class="fas fa-box"></i>　商　品
+                        </b-button>
+                        <b-button variant="primary" block href="/invoice-page"><i
+                                class="fas fa-copy"></i>　請求書</b-button>
+                        <b-button variant="primary" block href="/quotation-page"><i
+                                class="far fa-copy"></i>　見積書</b-button>
+                        <b-button variant="primary" block href="/memo-page"><i
+                                class="fas fa-book-open"></i>　メ　モ</b-button>
+                    </b-nav>
+                </nav>
+            </div>
+        </template>
+    </b-sidebar>
+    </div>
+    `,
+    props: []
+});
